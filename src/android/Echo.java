614c392926bd74c906a9abaaaaf0b3fa87b66c53
Timo van der Schuit <timoteus78@gmail.com>
2014-01-30 08:24:10 +0000

    added import import org.apache.cordova.CallbackContext

--- src/android/Echo.java
+++ src/android/Echo.java
@@ -1,6 +1,7 @@
 package org.apache.cordova.plugin;
 
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
